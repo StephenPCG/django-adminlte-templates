49897b7ef1e8cef7dc384e2e066ae2cdc2be836e
Stephen Zhang <stephenpcg@gmail.com>
2015-07-04 13:21:36 +0800

    Add an example django project for demonstration

---
+++ example/example/adminlte_settings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+ADMINLTE_SETTINGS = {
+    'LOGIN': {
+        'LOGO': r'<b>Example</b> Site',
+    },
+    'REGISTER': {
+        'LOGO': r'<b>Example</b> Site',
+    },
+}
